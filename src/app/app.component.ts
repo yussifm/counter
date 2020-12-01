670d37809020be1c1bc95d367a39c6fcbe3e7ac1
yussif mohammed <43506617+yussifm@users.noreply.github.com>
2020-12-01 03:56:10 +0000

    2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,15 @@ export class AppComponent {
   title = 'counter';
   counter = 0;
 
+  increase(): number {
+    return this.counter++;
+}
+  decrease(): number{
+    return this.counter--;
+  }
 
+  reset(): number {
+    return this.counter = 0;
+  }
 
 }
